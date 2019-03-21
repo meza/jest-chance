240ace1654d97b529011016729615662b3b46b89
meza <meza@meza.hu>
2019-03-21 12:22:39 +0000

    added types

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,5 +2,6 @@
 // Project: https://github.com/baz/foo (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
 // Definitions by: My Self <https://github.com/me>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+/// <reference types="Chance" />
 
-export function jestThing(): void;
+export const chance: Chance.Chance;
