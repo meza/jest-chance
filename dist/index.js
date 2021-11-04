715cfc908af67cb2e3678a1e5089b673a6021ba1
meza <meza@meza.hu>
2021-11-04 23:41:07 +0000

    chore(version): upgraded versions

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.chance = exports.getChance = void 0;
 const chance_1 = __importDefault(require("chance"));
 const getSeed = () => {
     if (!process.env.CHANCE_SEED) {
